161004abe1cb28d7ee28ecf559a3250d89bca56d
Kevin Sajan James <kevin@kryil.com>
2023-06-13 14:06:12 +0400

    first commit!

---
+++ crud.py
@@ -0,0 +1,45 @@
+from sqlalchemy.orm import Session
+
+import models, schemas
+
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+
+def create_user(db: Session, user: schemas.UserCreate):
+    fake_hashed_password = user.password + "notreallyhashed"
+    db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def get_items(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Item).offset(skip).limit(limit).all()
+
+
+def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
+    db_item = models.Item(**item.dict(), owner_id=user_id)
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+########################################################################################################################
+
+def create_invoice(db: Session, invoice: schemas.Invoice):
+    db_invoice = models.User(id=invoice.id, product=invoice.product)
+    db.add(db_invoice)
+    db.commit()
+    db.refresh(db_invoice) 
+    return db_invoice
