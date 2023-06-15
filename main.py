b731368071569be0abf7c1557918b0b7348e805d
Kevin Sajan James <kevin@kryil.com>
2023-06-15 11:39:37 +0400

    basic CRUD working!

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, status, Response
 from sqlalchemy.orm import Session
 import crud, models, schemas
 from database import SessionLocal, engine
@@ -51,37 +51,45 @@ def get_db():
 #     return items
 
 #########################################################################################################################
+@app.get("/api/invoice/all")
+def get_all_invoices(db: Session = Depends(get_db)):
+    return db.query(models.Invoice).all()
+
+@app.post("/api/invoice/create", response_model=schemas.Invoice, status_code=status.HTTP_201_CREATED)
+def create_invoice(request: schemas.InvoiceCreate, db: Session = Depends(get_db)):
+    invoice = models.Invoice(product=request.product)
+    db.add(invoice)
+    db.commit()
+    db.refresh(invoice) 
+    return invoice
 
 @app.put("/api/invoice/{id}/update", response_model=schemas.Invoice)
 def update_invoice(id: int, request: schemas.InvoiceCreate, db: Session = Depends(get_db)):
     invoice = db.query(models.Invoice).get(id)
+    if not invoice:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
     if request.product:
         invoice.product = request.product
     db.commit()
     return invoice
 
-@app.delete("/api/invoice/{id}/delete", response_model=schemas.InvoiceCreate)
+@app.delete("/api/invoice/{id}/delete")
 def delete_invoice(id: int, db: Session = Depends(get_db)):
     invoice = db.query(models.Invoice).get(id)
-    db.delete(invoice)
+    db.query(models.Invoice).filter(models.Invoice.id==id).delete(synchronize_session=False)
+    if not invoice:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    # db.delete(invoice)
     db.commit()
     return {'msg':'Invoice Deleted Successfully!'}
     
 @app.get("/api/invoice/{id}", response_model=schemas.Invoice)
 def get_invoice_by_id(id: int, db: Session = Depends(get_db)):
-    return db.query(models.Invoice).filter(models.Invoice.id==id).first()
+    invoice = db.query(models.Invoice).get(id)
+    if not invoice:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    return invoice
 
-@app.post("/api/invoice/create", response_model=schemas.Invoice)
-def create_invoice(request: schemas.InvoiceCreate, db: Session = Depends(get_db)):
-    db_invoice = models.Invoice(product=request.product)
-    db.add(db_invoice)
-    db.commit()
-    db.refresh(db_invoice) 
-    return db_invoice
-
-@app.get("/api/invoice/all")
-def get_all_invoices(db: Session = Depends(get_db)):
-    return db.query(models.Invoice).all()
 
 
 
